e65c2a634ef0319c7b438f94b26b8a42cf750d6b
Ciaran Whitty <whittyciaran1998@gmail.com>
2022-07-10 00:00:20 +0100

    Add Id's To Table

--- RetrieveTableData.php
+++ RetrieveTableData.php
@@ -12,11 +12,18 @@
 		or die("ERROR: Selecting Data. " . mysqli_connect_error());
 
 	echo "<table border='1'>";
-	echo "<tr> <th>Name</th> <th>Age</th> </tr>";
+	echo 
+	"<tr> 
+	<th>Id</th> 
+	<th>Name</th> 
+	<th>Age</th> 
+	</tr>";
 	//keeps getting the next row until there are no more to get 
 	while($row = mysqli_fetch_array( $result )) {
 	// Print out the contents of each row into a table
 	echo "<tr><td>"; 
+		echo $row['id'];
+	echo "</td><td>"; 
 		echo $row['name'];
 	echo "</td><td>";
 		echo $row['age'];
--- index.php
+++ index.php
@@ -2,7 +2,6 @@
   include_once "./components/header.php";
 ?>
 
-
 <?php
   include_once "./components/footer.php";
 ?>
